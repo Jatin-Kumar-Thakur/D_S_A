41b957967a9a9da1dfa45cbe7aa0ca95746dbfb0
Jatin kumar <109894649+Jatin-Kumar-Thakur@users.noreply.github.com>
2023-02-22 00:22:23 +0530

    Update 2_create_BST.cpp

--- 8_Tree/2_create_BST.cpp
+++ 8_Tree/2_create_BST.cpp
@@ -4,65 +4,60 @@ using namespace std;
 class node
 {
 public:
-    int data;
-    node *left;
-    node *right;
-    node(int data)
-    {
-        this->data = data;
-        this->left = 0;
-        this->right = 0;
-    }
+	int data;
+	node *left;
+	node *right;
+	node(int data)
+	{
+		this->data = data;
+		this->left = 0;
+		this->right = 0;
+	}
 };
 
-node *bst(node *&root)
+node *bst(node *root, int data)
 {
-    int data;
-    while (true)
-    {
-        cout << "Enter Data : ";
-        cin >> data;
-        if (data == -1)
-        {
-            break;
-        }
-        root = new node(data);
-        // if (root == 0)
-        // {
-        //    root =root;
-        // }
-        //else
-        if (data < root->data)
-        {
-            root->left = bst(root->left);
-        }
-        else
-        {
-            root->right = bst(root->right);
-        }
-        return root;
-    }
-    return root;
+
+	if (root == NULL)
+	{
+		return new node(data);
+	}
+	if (data < root->data)
+	{
+		root->left = bst(root->left, data);
+	}
+	else
+	{
+		root->right = bst(root->right, data);
+	}
+
+	return root;
 }
 
 void inorder(node *root)
 {
-    if (root == nullptr)
-    {
-        return;
-    }
-    inorder(root->left);
-    cout << root->data << " ";
-    inorder(root->right);
+	if (root == nullptr)
+	{
+		return;
+	}
+	inorder(root->left);
+	cout << root->data << " ";
+	inorder(root->right);
 }
 
 int main(int argc, char const *argv[])
 {
-    node *root = 0;
-    root = bst(root);
-    // root = bst(root);
-    // root = bst(root);
-    cout << "Data: ";
-    inorder(root);
-    return 0;
+	node *root = NULL;
+	int data = 0;
+	cout << "Enter data : ";
+	cin >> data;
+	while (data != -1)
+	{
+		root = bst(root, data);
+		cout << "Enter data : ";
+		cin >> data;
+	}
+	cout << "Data: ";
+	inorder(root);
+	return 0;
 }
